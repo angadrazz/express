cdd6851680afe7b11efdb42c2a6faf6a5076a3d4
angadrazz <angadraj9534@gmail.com>
2023-03-29 11:35:27 +0530

    working fine server.js

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port =process.env.PORT || 5000;
 
+app.get('/api/contacts', (req, res) => {
+        res.status(201).json({message: "See your all contact"});
+})
+
 app.listen(port, () => {
         console.log(`Server running on port ${port}`);
 })
