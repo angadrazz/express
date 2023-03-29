c75bb9848895e9ba65628d52927b4767a940cbe1
angadrazz <angadraj9534@gmail.com>
2023-03-29 11:03:00 +0530

    defined

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,9 +1,1 @@
-const express = require("express");
-const app = express();
- const dotenv = require("dotenv").config();
-
-const port = process.env.PORT || 5000;
-
-app.use("/api/contacts", require("./Routes/ContactRoutes"));
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+console.log("hello");
