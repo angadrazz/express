59023b96283b922f720e97679e63d774e2a42aeb
angadrazz <angadraj9534@gmail.com>
2023-03-29 12:05:04 +0530

    contact routes

---
+++ mycontacts-backend/routes/contactRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/").get((req, res) => {
+        res.status(200).json({message: "See your all contact"});
+});
+
+router.route("/").post((req, res) => {
+        res.status(200).json({message: "Create contact"});
+});
+
+router.route("/:id").get((req, res) => {
+        res.status(200).json({message: `Update contact for ${req.params.id}`});
+});
+
+router.route("/:id").put((req, res) => {
+        res.status(200).json({message: `Update contact for ${req.params.id}`});
+});
+
+router.route("/:id").delete((req, res) => {
+        res.status(200).json({message: `Delete contact for ${req.params.id}`});
+});
+
+module.exports = router;
