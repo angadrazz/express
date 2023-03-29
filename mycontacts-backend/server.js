37afcf45febdf013ac891e594018e154f9482055
angadrazz <angadraj9534@gmail.com>
2023-03-29 11:09:47 +0530

    Server running on port ${port}`

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+})
