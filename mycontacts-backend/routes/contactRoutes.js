db7b8b9dbe75b287790996561de9937f8105edc3
angadrazz <angadraj9534@gmail.com>
2023-03-29 13:06:17 +0530

    updates contact routes

--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { getContacts, 
+        createContact, 
+        getContact, 
+        updateContact, 
+        deleteContact
+} = require("../controllers/contactController");
 
-router.route("/").get((req, res) => {
-        res.status(200).json({message: "See your all contact"});
-});
-
-router.route("/").post((req, res) => {
-        res.status(200).json({message: "Create contact"});
-});
-
-router.route("/:id").get((req, res) => {
-        res.status(200).json({message: `Update contact for ${req.params.id}`});
-});
-
-router.route("/:id").put((req, res) => {
-        res.status(200).json({message: `Update contact for ${req.params.id}`});
-});
-
-router.route("/:id").delete((req, res) => {
-        res.status(200).json({message: `Delete contact for ${req.params.id}`});
-});
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
