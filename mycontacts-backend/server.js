0aa3878196de088d0d40dc9ab6f38a6335dac300
angadrazz <angadraj9534@gmail.com>
2023-03-29 15:48:34 +0530

    settings updated

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandling = require("./middleware/errorHandling");
 const dotenv = require("dotenv").config();
 const app = express();
 
@@ -6,6 +7,7 @@ const port =process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandling);
 
 app.listen(port, () => {
         console.log(`Server running on port ${port}`);
