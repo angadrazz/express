5eb631e8bafd4d2db6ad4032cf45d6f5e0ba732e
angadrazz <angadraj9534@gmail.com>
2023-04-01 09:42:00 +0530

    app crashed

--- mycontacts-backend/config/dbConnection.js
+++ mycontacts-backend/config/dbConnection.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
+        try {
+          const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+          console.log("Database Connected: ", connect.connection.host);
+        } catch (error) {
+          console.log(error);
+          process.exit(1);
+        }
+      };
+      
 
-}
+module.exports = connectDb;
